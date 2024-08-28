2fec10d87fb46b4460418fa4df2243bd36e7e9b0
John Lu <z52lu@uwaterloo.ca>
2024-08-28 11:26:27 -0400

    use python wheels for shipment

--- main.py
+++ main.py
@@ -52,6 +52,9 @@ def main():
         print(f"The provided btor2 file does not exist: {btor2_path}")
         return
     btor2select_dir = os.path.dirname(this_script_path)
+    wheels_dir = Path(btor2select_dir) / "wheels"
+    for wheel in wheels_dir.glob("*.whl"):
+        sys.path.append(str(wheel))
     saved_model_dir = Path(btor2select_dir) / "pw_xg_par2_0826"
     tool_dict_json = Path(btor2select_dir) / "tool_config_dict.json"
     with open(tool_dict_json, 'r') as f:
