e2ff353a39f4f439666f70d4d524fb4fa0cf3c99
Po-Chun Chien <po-chun.chien@sosy.ifi.lmu.de>
2024-08-30 19:14:14 +0200

    Make script executable and do not create archive automatically

--- install_pack.sh
+++ install_pack.sh
@@ -7,4 +7,4 @@ mkdir lib
 pip3.10 install joblib==1.4.2 --target=./lib
 pip3.10 install numpy==1.26.4 --target=./lib
 pip3.10 install xgboost==2.0.3 --target=./lib
-tar -czvf ../btor2select_pw.tar.gz .
+# tar -czvf ../btor2select_pw.tar.gz .
